687e8077ab4c67cde5bcdbbebf29ce8f9bcde1c1
Juan Cuenu <31332546+cuenujp@users.noreply.github.com>
2017-08-28 19:17:41 -0500

    version1

---
+++ taller2/ejem-Log_add_data_to_archivos/src/logica/ArchivoLog.java
@@ -0,0 +1,65 @@
+package logica;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Calendar;
+
+
+public class ArchivoLog{
+
+	FileWriter archivo;
+	
+
+     public void crearLog(String operacion) {
+
+   		if (new File("log.txt").exists() == false){
+			try {
+				archivo = new FileWriter(new File("log.txt"),false);
+				System.out.println("se creo el archivo");
+				this.imprimir(" -ejecutando crearLog- ");
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("error creando el archivo existente "+ e);
+			}
+        	}
+		else{
+			try {
+				
+				this.imprimir(" -ejecutando metodo imprimir- ");
+				
+				System.out.println("se agrego informacion en el archivo");
+								
+			} catch (Exception e) {
+				// TODO: handle exception
+				System.out.println("error escribiendo en nuevo archivo");
+				
+			}
+		}
+     }
+    
+	@crearLog
+     public void agregarLog(String linea) {
+		  this.imprimir(" -ejcutando metodoagregarLog- ");
+          this.crearLog(" -ejecucuion metodo CrearLog- "+linea); //Guarda en el Log el evento realizado
+          System.out.println(" linea agregada al archivo");
+     }
+
+	@crearLog
+    public void imprimir(String operacion){
+    	try {
+    		archivo = new FileWriter(new File("log.txt"), true);
+    		Calendar fechaActual = Calendar.getInstance(); 
+    		archivo.write((String.valueOf(fechaActual.get(Calendar.DAY_OF_MONTH))
+    				+"/"+String.valueOf(fechaActual.get(Calendar.MONTH)+1)
+    				+"/"+String.valueOf(fechaActual.get(Calendar.YEAR))
+    				+";"+String.valueOf(fechaActual.get(Calendar.HOUR_OF_DAY))
+    				+":"+String.valueOf(fechaActual.get(Calendar.MINUTE))
+    				+":"+String.valueOf(fechaActual.get(Calendar.SECOND)))+"; "+operacion+"\r\n");
+    		archivo.close(); //Se cierra el archivo
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+    }
+
+}
---
+++ taller2/ejem-Log_add_data_to_archivos/src/test/main.java
@@ -0,0 +1,14 @@
+package test;
+
+import logica.ArchivoLog;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 ArchivoLog archivo = new ArchivoLog(); 
+	     archivo.agregarLog(" ejecucuion del main");
+	     
+	}
+
+}
