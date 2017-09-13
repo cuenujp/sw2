292a8aeeb5e008c6d23ec2a44c93517fe0ac2e8a
Juan Cuenu <31332546+cuenujp@users.noreply.github.com>
2017-09-12 20:49:53 -0500

    filtro login

---
+++ Taller3/src/java/logica/FiltroLogin.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Cuenu
+ */
+@WebFilter(filterName = "FiltroLogin", urlPatterns = {"/login.html"})
+public class FiltroLogin implements Filter{
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp =(HttpServletResponse) response;
+        if (req.getSession().getAttribute("usuario") == req.getSession().getAttribute("contraseña")) {
+            System.out.println("logica.FiltroLogin.doFilter()");
+            resp.sendRedirect("./bienvenido.html");
+        } else {
+            chain.doFilter(request, response);
+        }
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void destroy() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
